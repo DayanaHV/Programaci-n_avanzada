686b13193d65f692b0d6d965c78ad197633fc005
DayanaHV <117460854+DayanaHV@users.noreply.github.com>
2022-11-25 08:58:31 -0500

    Update dayana.py

--- dayana.py
+++ dayana.py
@@ -18,6 +18,7 @@ st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' c
 st.dataframe(c)
 st.subheader("Características del Dataset")
 st.write(c.describe())
+
 df=c
 filt=(df["EDAD_DECLARADA"]==año_seleccionado)
 df[filt]
