6f21c41a96c94fbace430d0064087e59dde83b48
DayanaHV <117460854+DayanaHV@users.noreply.github.com>
2022-11-25 09:56:09 -0500

    Update dayana.py

--- dayana.py
+++ dayana.py
@@ -23,13 +23,8 @@ año_seleccionado=st.sidebar.selectbox('Edad', list(reversed(range(0,111))))
 filt=(df["EDAD_DECLARADA"]==año_seleccionado)
 df[filt]
 
-departamentos=["AMAZONAS","ANCASH","APURIMAC","AREQUIPA","AYACUCHO","CAJAMARCA","CALLAO","CUSCO","HUANCAVELICA","HUANUCO","ICA","JUNÍN","LA LIBERTAD","LAMBAYEQUE","LIMA","LORETO","MADRE DE DIOS","MOQUEGUA","PASCO","PIURA","PUNO","SAN MARTÍN","TACNA","TUMBES","UCAYALI"]
-depa_seleccionado=st.sidebar.multiselect('Departamentos',departamentos,departamentos)
-filt=(df["DEPARTAMENTO"]==depa_seleccionado)
-df[filt]
-
 sexo=["MASCULINO","FEMENINO"]
 sexo_seleccionado=st.sidebar.selectbox("Sexo",sexo,sexo)
-filt=(df["DEPARTAMENTO"]==sexo_seleccionado)
+filt=(df["SEXO"]==sexo_seleccionado)
 df[filt]
 
