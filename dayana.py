3758099ceb5df6d4ef37ae054528bade90e35c42
DayanaHV <117460854+DayanaHV@users.noreply.github.com>
2022-11-25 09:14:47 -0500

    Update dayana.py

--- dayana.py
+++ dayana.py
@@ -7,13 +7,17 @@ import altair as alt
 st.sidebar.header("Entradas del usuario")
 año_seleccionado=st.sidebar.selectbox('Edad', list(reversed(range(0,117))))
 st.header("Conjunto de datos FALLECIDOS COVID")
+
 @st.experimental_memo
 def download_data():
    url="https://raw.githubusercontent.com/DayanaHV/Programaci-n_avanzada/main/fallecidos_covid.csv"
    df=pd.read_csv("fallecidos_covid.csv")
    return df
 c=download_data()
+
 st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
 st.dataframe(c)
-st.subheader("Características del Dataset")
-st.write(c.describe())
+
+df=c
+filt=(df["EDAD_DECLARADA"]==año_seleccionado)
+df[filt]
