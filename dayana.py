5b622c9d7fc1160c6b08785b10fdb8fe69e99069
DayanaHV <117460854+DayanaHV@users.noreply.github.com>
2022-11-25 09:25:29 -0500

    Update dayana.py

--- dayana.py
+++ dayana.py
@@ -4,9 +4,6 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
-st.sidebar.header("Entradas del usuario")
-año_seleccionado=st.sidebar.selectbox('Edad', list(reversed(range(0,110))))
-
 st.header("Conjunto de datos FALLECIDOS COVID")
 
 @st.experimental_memo
@@ -19,11 +16,14 @@ c=download_data()
 st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
 st.dataframe(c)
 
-#df=c
-#filt=(df["EDAD_DECLARADA"]==año_seleccionado)
-#df[filt]
+df=c
+
+st.sidebar.header("Entradas del usuario")
+año_seleccionado=st.sidebar.selectbox('Edad', list(reversed(range(0,110))))
+filt=(df["EDAD_DECLARADA"]==año_seleccionado)
+df[filt]
 
 depa=["AMAZONAS","ANCASH","APURIMAC","AREQUIPA","AYACUCHO","CAJAMARCA","CALLAO","CUSCO","HUANCAVELICA","HUANUCO","ICA","JUNÍN","LA LIBERTAD","LAMBAYEQUE","LIMA","LORETO","MADRE DE DIOS","MOQUEGUA","PASCO","PIURA","PUNO","SAN MARTÍN","TACNA","TUMBES","UCAYALI"]
-depa_seleccionado=st.sidebar.multiselect('Departamento',depa)
+año_seleccionado=st.sidebar.selectbox('Edad',depa)
 filt=(df["DEPARTAMENTO"]==depa_seleccionado)
 df[filt]
