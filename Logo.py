9ff520cdd1f6b3d941d789b6f77f9ddcbfe72e7b
sebastianfranco1342 <117460313+sebastianfranco1342@users.noreply.github.com>
2022-12-02 08:31:04 -0500

    Create Logo.py

---
+++ Logo.py
@@ -0,0 +1,7 @@
+[data-testid="stSidebar"] {
+  background-image: url(https://eva.upch.edu.pe/pluginfile.php/316165/course/summary/logoxs_faest-upch.png);
+  background-size: 250px;
+  background-repeat: no-repeat;
+  background-position: 4px 20px;
+}
+  
