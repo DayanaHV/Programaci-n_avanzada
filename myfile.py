922fc4cd0f82be23269e9a1139ab36e81674de0c
sebastianfranco1342 <117460313+sebastianfranco1342@users.noreply.github.com>
2022-12-02 09:13:32 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -7,6 +7,10 @@ import pandas as pd
 from PIL import Image
 import numpy as np
 
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+with open('Logo.py') as f:
+	  st.markdown(f'<style>{f.read()}</Logo>', unsafe_allow_html=True)
+
 #TITULO "FALLECIDOS POR COVID-19"
 st.title('Fallecidos por COVID-19 - [Ministerio de Salud - MINSA]')
 st.markdown("**PROYECTO FINAL PROGRAMACIÓN 2022-2**")
@@ -91,9 +95,9 @@ st.subheader("Características del Dataset")
 st.write(c.describe())
 
 #------------------------------------------------------------------------------------------------------------------------------------
-st.set_page_config(layout='wide', initial_sidebar_state='expanded')
-with open('Logo.css') as f:
-	  st.markdown(f'<style>{f.read()}</Logo>', unsafe_allow_html=True)
+#st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+#with open('Logo.css') as f:
+	 # st.markdown(f'<style>{f.read()}</Logo>', unsafe_allow_html=True)
 #ENTRADA DEL USUARIO
 st.sidebar.header("Entradas del usuario")
 #FILTRO DE RANGO DE EDAD_DECLARADA 
